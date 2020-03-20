b5dd9c01001bbd5d0bdac6b04b0e0ec1718e7246
Adithi Satish <45226748+adithisatish@users.noreply.github.com>
2020-03-20 12:38:46 +0530

    Six insights done

--- test.py
+++ test.py
@@ -9,12 +9,13 @@ import os
 filepath = os.path.join(os.getcwd(),'CSV_Files','ninja_reports.xls')
 df = pd.read_excel('ninja_reports2.xls',sheet_name='ninja_reports')
 
+df_w = pd.read_csv("ward_details.csv")
+
 # %%
 df.head(40)
 
 # %%
-city_ids = list(df.city_id.unique())
-print(city_ids)
+
 
 # %%
 df.count()
@@ -23,16 +24,17 @@ df.count()
 df = df.drop(columns = ['jg_sub_category','title_id'])
 
 # %%
-#Correlation Matrix
-corr = df.corr()
-plt.figure(num=None, figsize=(7, 6), dpi=80, facecolor='w', edgecolor='k')
-corrMat = plt.matshow(corr, fignum = 1)
-plt.xticks(range(len(corr.columns)), corr.columns, rotation=90)
-plt.yticks(range(len(corr.columns)), corr.columns)
-plt.gca().xaxis.tick_bottom()
-plt.colorbar(corrMat)
-plt.title(f'Correlation Matrix', fontsize=15)
-plt.show()
+def cor_mat():
+    #Correlation Matrix
+    corr = df.corr()
+    plt.figure(num=None, figsize=(7, 6), dpi=80, facecolor='w', edgecolor='k')
+    corrMat = plt.matshow(corr, fignum = 1)
+    plt.xticks(range(len(corr.columns)), corr.columns, rotation=90)
+    plt.yticks(range(len(corr.columns)), corr.columns)
+    plt.gca().xaxis.tick_bottom()
+    plt.colorbar(corrMat)
+    plt.title(f'Correlation Matrix', fontsize=15)
+    plt.show()
 
 
 # %%
@@ -43,18 +45,36 @@ def isint(n):
     except:
         return -1;
 
-ward_count = 0
-for i in df.ward_id:
-    if(i<=198):
-        ward_count+=1
-print("Noise: ",ward_count)
+def ward():
+    l = list(df.ward_id.unique())
+    len(l)
+    ward_count = 0
+    for i in df.ward_id:
+        if(i<=198):
+            ward_count+=1
+    print("Noise: ",ward_count)
 
 # %%
-
+city_number = []
+def new_city_id():
+    blor = ["chennai"] #['bangalore', 'Bangalore', 'bengaluru', 'Bengaluru', 'BANGALORE', 'BENGALURU']
+    i = 0
+    for city in df.location:
+        #print(str(city))
+        if "chennai" in str(city).lower():
+            city_number.insert(i, 2)
+        elif "hosur" in str(city).lower():
+            city_number.insert(i,3)
+        else:
+            city_number.insert(i,1)
+    df['city_number'] = city_number
+new_city_id()
 
 # %%
-l = list(df.ward_id.unique())
-len(l)
+plt.ylim(0,7000)
+plt.title("Cities")
+plt.grid(True)
+plt.bar(['Bangalore','Chennai','Hosur'],[city_number.count(1),city_number.count(2),city_number.count(3)])
 
 # %%
 c_count = 0
@@ -64,8 +84,33 @@ for i in df.location:
 print("Count : ",c_count)
 
 # %%
-title = list(df.title.unique())
-print(title,len(title))
+def new_title():
+    new_title_id = []
+    i = 0
+    for name in df.title:
+        if name in ['Garbage', 'Garbage Dumping In Vacant Lot/Land', 'Implementation Of Waste Segregation/Garbage', 'Clearance Of Garbage Dump Or Black Spot', 'Clearing Of Roadside Dustbin', 'Collection Of Door-to-door Garbage', 'BPAC – Black Spot']:
+            new_title_id.insert(i, 'Waste/Garbage')
+        elif name in ['Air Pollution']:
+            new_title_id.insert(i, 'Air')
+        elif name in ['Maintenance/Repair Of Streetlights', 'Dangling Or Hanging Of OFCs/Cables/Wires', 'Removal Of Illegal Posters And Hoardings']:
+            new_title_id.insert(i, 'Streetlights')
+        elif name in ['Regular Supply Of Electricity']:
+            new_title_id.insert(i, 'Electricity')
+        elif name in ['Maintenance And Repair Of Manholes', 'Fixing/Reparing Potholes']:
+            new_title_id.insert(i, 'Potholes')
+        elif name in ['Traffic Jams/Congestion Or Bottlenecks','Wrong Parking', 'Wrong parking', 'Parking On Footpath']:
+            new_title_id.insert(i, 'Traffic/Parking')
+        elif name in ['Provide Sewage lines', 'Maintenance And Repair Of Sewage Lines']:
+            new_title_id.insert(i, 'Sewage')
+        elif name in ['Stop Water Leakage', 'Flooding/Waterlogging Of Roads And Footpaths','Maintenance Of Lake Surrounding']:
+            new_title_id.insert(i, 'Water')
+        elif name in ['Single Use plastic generator']:
+            new_title_id.insert(i, 'Plastic')
+        else:
+            new_title_id.insert(i, 'Other')
+        i = i+1
+    df['new_title_id'] = new_title_id
+new_title()
 
 # %%
 cat = {}
@@ -76,47 +121,71 @@ for i in df.category:
     cat[i] +=1
 
 print(cat)
-plt.figure(figsize = (7,7))
+plt.figure(figsize = (8,8))
 plt.title("Categories")
 plt.pie(cat.values(),labels = cat.keys(),autopct = '%.2f')
 
 # %%
+def bar_new_cat():
+    new_cat = {}
+    for i in df.new_title_id:
+        if i not in new_cat:
+            new_cat[i] = 1
+        else:
+            new_cat[i]+=1
+    plt.figure(figsize = (8,8))
+    plt.title("Categories - New")
+    #plt.grid(True)
+    print(new_cat)
+    plt.pie(new_cat.values(),labels = new_cat.keys(),  autopct = '%.2f')# color = ['green','red'], tick_label = df.new_title_id.unique())
 
+bar_new_cat()
+
+# %%
 
 
 # %%
-chennai = [str(i) for i in df.location if 'Chennai' in str(i)]
-for i in chennai:
-    df.drop(d)
+
 
 # %%
 
 
 # %%
-for i in df.category:
-    cat[i] +=1
+def date_plot():
+    df.createdAt = pd.to_datetime(df['createdAt'])
+    plt.figure(figsize=(13,7))
+    plt.title("Complaint Dates: During what time were most complaints made?")
+    plt.xlabel('Dates')
+    plt.grid(True)
+    plt.ylabel('Count')
+    plt.hist(df.createdAt,color='purple')
 
-print(cat)
+def date_plot_july():
+    dates = []
+    for i in df.createdAt:
+        d1 = str(i).split(' ')
+        d = d1[0].split('-')
+        if d[1] == '07':
+            dates.append(d1[0])
+    date = {}
+    for i in dates:
+        if i not in date:
+            date[i] = 1
+        else:
+            date[i]+=1
+    #print(date['2019-10-02'])
+    plt.figure(figsize = (38,16))
+    plt.title("Date Plot (July 2019)")
+    plt.grid(True)
+    plt.ylim(0,300)
+    plt.plot_date(date.keys(),date.values(),color = 'red')
+#plt.hist(dates)
+
+date_plot()
+date_plot_july()
 
 # %%
-df.createdAt = pd.to_datetime(df['createdAt'])
-plt.figure(figsize=(13,7))
-plt.title("Complaint Dates: During what time were most complaints made?")
-plt.xlabel('Dates')
-plt.grid(True)
-plt.ylabel('Count')
-plt.hist(df.createdAt,color='purple')
 
-# %%
-dates = []
-for i in df.createdAt:
-    d = str(i).split('-')
-    if d[1] == '08':
-        dates.append(str(i))
-    elif (d[1] == '07' or d[1] == '09'):
-        d[1] = d[1].split(' ')
-        if(int(d[1][0]) >=15 and int(d[1][0]) <= 31):
-            dates.append(str(i))
 
 # %%
 
@@ -126,70 +195,130 @@ print(df.status_id.unique())
 
 # %%
 stat = {}
-for i in df.status_id:
-    if i not in stat:
-        stat[i] = 1
-    else:
-        stat[i]+=1
-print(stat)
-plt.figure(figsize = (8,8))
-plt.pie(stat.values(),labels = stat.keys(),autopct = "%.2f")
-
-# %%
-print("The status IDs : ",stat)
-plt.figure(figsize = (7,6))
-plt.title("Status ID bar chart")
-plt.grid(True)
-plt.xlabel("Status IDs")
-plt.ylim(0,7000)
-plt.bar(stat.keys(), height=stat.values(), color = 'darkblue')
-
-# %%
-df1 = df[['category','status_id']]
-#print(df1)
-#for i in df1.categor
-cat = list(df.category)
-stat = list(df.status_id)
-res = {}
-for i in stat:
-    if i==3:
-        ct = cat[stat.index(i)]
-        if ct not in res:
-            res[ct] = 1
+def stat_id_pie(): 
+    for i in df.status_id:
+        if i not in stat:
+            stat[i] = 1
         else:
-            res[ct]+=1
-print(res)
-plt.figure(figsize = (5,5))
-plt.title("Categories of complaints resolved")
-plt.pie(res.values(),labels = res.keys(),autopct = '%.2f')
+            stat[i]+=1
+    #print(stat)
+    plt.figure(figsize = (8,8))
+    plt.title("Status IDs")
+    plt.pie(stat.values(),labels = stat.keys(),autopct = "%.2f")
+
+def stat_id_bar():
+    print("The status IDs : ",stat)
+    plt.figure(figsize = (7,6))
+    plt.title("Status ID bar chart")
+    plt.grid(True)
+    plt.xlabel("Status IDs")
+    plt.ylim(0,7000)
+    plt.bar(stat.keys(), height=stat.values(), color = 'darkblue')
+    
+stat_id_pie()
+stat_id_bar()
 
 # %%
-dates = []
-for i in df.createdAt:
-    d1 = str(i).split(' ')
-    d = d1[0].split('-')
-    if d[1] == '07':
-        dates.append(d1[0])
-    elif (d[1] == '08'):
-        #d[1] = d[1].split(' ')
-        if(int(d[2]) <=5):
-            dates.append(d1[0])
-    elif(d[1]=='10' and d[2]=='02'):
-        dates.append(d1[0])
-date = {}
-for i in dates:
-    if i not in date:
-        date[i] = 1
-    else:
-        date[i]+=1
-print(date['2019-10-02'])
-plt.figure(figsize = (50,16))
-plt.grid(True)
-plt.ylim(0,300)
-plt.plot_date(date.keys(),date.values())
-#plt.hist(dates)
+
 
 # %%
-plt.plot(date.keys(),date.values())
+def stat_resolved():
+    df1 = df[['new_title_id','status_id']]
+    #print(df1)
+    #for i in df1.categor
+    cat = list(df.new_title_id)
+    stat = list(df.status_id)
+    res = {}
+    for i in stat:
+        if i==3:
+            ct = cat[stat.index(i)]
+            if ct not in res:
+                res[ct] = 1
+            else:
+                res[ct]+=1
+    print(res)
+    plt.figure(figsize = (5,5))
+    plt.title("Categories of complaints resolved")
+    plt.pie(res.values(),labels = res.keys(),autopct = '%.2f')
+stat_resolved()
+
+# %%
+
+
+# %%
+
+
+# %%
+
+
+# %%
+fig, axes = plt.subplots(figsize=(6,6))
+axes.scatter(df.longitude, df.latitude, s=0.1, alpha=0.5, c='r')
+plt.show()
+
+
+# %%
+bangalore_map_img = 'https://lh3.googleusercontent.com/np8igtYRrHpe7rvJwMzVhbyUZC4Npgx5fRznofRoLVhP6zcdBW9tfD5bC4FbF2ITctElCtBrOn7VH_qEBZMVoPrTFipBdodufT0QU1NeeQVyokMAKtvSHS9BfYMswXodz_IrkiZStg=w500-h664-no'
+bangalore_map = plt.imread(bangalore_map_img)
+cmap = plt.get_cmap("jet")
+
+axes = df.plot(figsize=(10,15), kind='scatter', 
+                    x='longitude', y='latitude', 
+                    alpha=0.5, marker="o", cmap=cmap,
+                    colorbar=False)
+
+epsilon = 0.01
+bound_box = [min(df.longitude) + epsilon, max(df.longitude) + epsilon, 
+             min(df.latitude) + epsilon, max(df.latitude) + epsilon]
+im = plt.imshow(bangalore_map, extent=bound_box, zorder=0, 
+           cmap=cmap, interpolation='nearest', alpha=0.7)
+'''
+axes.set_ylabel("Latitude")
+axes.set_xlabel("Longitude")
+axes.set_title('Accident Heatmap of city of Bangalore')
+# Colorbar
+speed = train_data["speed"].values
+tick_values = np.linspace(speed.min(), speed.max(), num=6, dtype=np.int64)
+
+cbar = plt.colorbar(im, fraction=0.05, pad=0.02)
+cbar.set_label("Speed (km / hour)")
+cbar.ax.set_yticklabels(["%d"%(val) for val in tick_values])
+
+plt.tight_layout()
+
+#output_image = os.path.join(input_data_dir, "output_bangalore_map_traffic")
+#plt.savefig(output_image + ".png", format='png', dpi=300)'''
+
+axes.set_ylabel("Latitude")
+axes.set_xlabel("Longitude")
+
+plt.tight_layout()
+
+plt.show()
+
+# %%
+print(min(abs(df.latitude)))
+
+# %%
+count = 0
+for i in df.latitude:
+    if int(i)==0:
+        count+=1
+print(count)
+
+# %%
+df_dict = df.to_dict()
+#df_dict
+count = 0
+for j in range(len(df['id'])):
+    if int(df_dict['latitude'][j])==0 and int(df_dict['longitude'][j])==0:
+        count+=1
+print(count)
+
+# %%
+#hdsf
+
+# %%
+
 
 # %%
